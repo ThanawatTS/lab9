c1688e9d596ac2ebebc7eeb8a02b7ff4d85884cb
GamTS <Spirit_windwalk@hotmail.com>
2016-04-22 20:47:09 +0700

    start

---
+++ src/Length.java
@@ -0,0 +1,19 @@
+
+
+public enum Length {
+
+	METER ("Meter", 1.0),
+	FOOT("Foot",0.3048),
+	MILE("Mile",1609.344),
+	INCH("Inch",0.0254),
+	KILOMETER("Kilometer",1000),
+	WA("Wa",2);
+	public final String name;
+	public final double value;
+	Length(String name , double value){
+	this.name =name;
+	this.value = value;
+	}
+	public double getValue(){return value;}
+	public String toString(){return name;}
+}

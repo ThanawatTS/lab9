21c59752ed19137098c55fecc647007262095bfa
GamTS <Spirit_windwalk@hotmail.com>
2016-04-23 19:29:02 +0700

    GUI Finished

--- src/ConverterUI.java
+++ src/ConverterUI.java
@@ -1,45 +1,80 @@
+import java.awt.EventQueue;
 
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.ActionEvent;
+import javax.swing.JFrame;
+import java.awt.FlowLayout;
+import javax.swing.JLabel;
+import javax.swing.JComboBox;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.JButton;
 
-public class ConverterUI extends JFrame {
-private UnitConverter unitconverter;
-private JButton convert,Clear;
-private JComboBox unit1ComboBox,unit2ComboBox;
-private JTextField Putnumber,result;
-private JLabel equal;
-private JFrame Title;
-private JPanel panel;
-//....
-public ConverterUI (UnitConverter uc){
-	this.unitconverter = uc;
-	this.setTitle("Length Converter");
-	this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-	initComponents();
-	
-}
-private void initComponents(){
-setVisible(true);
-panel = new JPanel();
-Title = new JFrame("Distance Converter");
-Putnumber = new JTextField(20);
-unit1ComboBox = new JComboBox();
-equal = new JLabel ("=");
-result = new JTextField ();
-unit2ComboBox = new JComboBox();
-convert = new JButton ("Convert!");
+public class ConverterUI {
 
-Clear = new JButton ("Clear");
-panel.add(Title);
-panel.add(Putnumber);
-panel.add(unit1ComboBox);
-panel.add(equal);
-panel.add(result);
-panel.add(unit2ComboBox);
-panel.add(convert);
-panel.add(Clear);
+	private JFrame frmDfd;
+	private JTextField textField;
+	private JTextField textField_1;
 
-	
-}
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					ConverterUI window = new ConverterUI();
+					window.frmDfd.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public ConverterUI() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frmDfd = new JFrame();
+		frmDfd.setTitle("Distance Converter");
+		frmDfd.setBounds(100, 100, 666, 76);
+		frmDfd.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmDfd.getContentPane().setLayout(null);
+		
+		textField = new JTextField();
+		textField.setBounds(10, 11, 86, 20);
+		frmDfd.getContentPane().add(textField);
+		textField.setColumns(10);
+		
+		JComboBox comboBox = new JComboBox();
+		comboBox.setBounds(106, 11, 110, 20);
+		frmDfd.getContentPane().add(comboBox);
+		
+		JLabel label = new JLabel("=");
+		label.setBounds(226, 14, 14, 14);
+		frmDfd.getContentPane().add(label);
+		
+		textField_1 = new JTextField();
+		textField_1.setBounds(250, 11, 97, 20);
+		frmDfd.getContentPane().add(textField_1);
+		textField_1.setColumns(10);
+		
+		JComboBox comboBox_1 = new JComboBox();
+		comboBox_1.setBounds(357, 11, 110, 20);
+		frmDfd.getContentPane().add(comboBox_1);
+		
+		JButton btnConvert = new JButton("Convert!");
+		btnConvert.setBounds(478, 10, 89, 23);
+		frmDfd.getContentPane().add(btnConvert);
+		
+		JButton btnClear = new JButton("Clear");
+		btnClear.setBounds(577, 10, 63, 23);
+		frmDfd.getContentPane().add(btnClear);
+	}
 }
---
+++ src/ConverterUIc.java
@@ -0,0 +1,45 @@
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+
+public class ConverterUIc extends JFrame {
+private UnitConverter unitconverter;
+private JButton convert,Clear;
+private JComboBox unit1ComboBox,unit2ComboBox;
+private JTextField Putnumber,result;
+private JLabel equal;
+private JFrame Title;
+private JPanel panel;
+//....
+public ConverterUI (UnitConverter uc){
+	this.unitconverter = uc;
+	this.setTitle("Length Converter");
+	this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+	initComponents();
+	
+}
+private void initComponents(){
+setVisible(true);
+panel = new JPanel();
+Title = new JFrame("Distance Converter");
+Putnumber = new JTextField(20);
+unit1ComboBox = new JComboBox();
+equal = new JLabel ("=");
+result = new JTextField ();
+unit2ComboBox = new JComboBox();
+convert = new JButton ("Convert!");
+
+Clear = new JButton ("Clear");
+panel.add(Title);
+panel.add(Putnumber);
+panel.add(unit1ComboBox);
+panel.add(equal);
+panel.add(result);
+panel.add(unit2ComboBox);
+panel.add(convert);
+panel.add(Clear);
+
+	
+}
+}
